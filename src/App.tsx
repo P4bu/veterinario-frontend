eba1e568bb3d488e94c9ca6fae7d4c2372c56bd8
Pablo Barra Urtubia <pbarrau.90@icloud.com>
2025-07-30 15:54:10 -0400

    Avanc: Pagina de login

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,8 @@
-import { Container, Typography, Button } from '@mui/material'
+import LoginPage from './pages/LoginPage'
 
 function App() {
   return (
-    <Container maxWidth="sm" sx={{ mt: 4 }}>
-      <Typography variant="h3" gutterBottom>
-        Veterinaria App
-      </Typography>
-      <Typography variant="body1" gutterBottom>
-        Gestiona tus pacientes y veterinarios desde aquí.
-      </Typography>
-      <Button variant="contained" color="primary">
-        Iniciar sesión
-      </Button>
-    </Container>
+    <LoginPage />
   )
 }
 
---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,87 @@
+import {
+  Box,
+  TextField,
+  Button,
+  Typography,
+  Paper,
+} from '@mui/material';
+import { useState } from 'react';
+
+const LoginPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log({ email, password });
+    // servicio login
+  };
+
+  return (
+    <Box sx={{ height: '98vh', display: 'flex' }}>
+
+      {/* imagen */}
+      <Box
+        sx={{
+          flex: 1,
+          display: { xs: 'none', sm: 'block' },
+          backgroundImage: 'url(/images/loginImage.jpg)',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+        }}
+      />
+
+      {/* formulario */}
+      <Box
+        component={Paper}
+        elevation={6}
+        square
+        sx={{
+          flex: 1,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          px: 4,
+        }}
+      >
+        <Box sx={{ width: '100%', maxWidth: 400 }}>
+          <Typography component="h1" variant="h5" textAlign="center" mb={2}>
+            Iniciar sesión
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleLogin}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              label="Correo electrónico"
+              autoComplete="email"
+              autoFocus
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              label="Contraseña"
+              type="password"
+              autoComplete="current-password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Ingresar
+            </Button>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default LoginPage;
